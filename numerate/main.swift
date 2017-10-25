b014ec9a468bd33bcf3376198f5cbae207c12854
David Grigg <git.user@rightword.com.au>
2017-10-26 07:28:20 +1100

    Initial Commit

---
+++ numerate/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  numerate
+//
+//  Created by David Grigg on 26/10/17.
+//  Copyright © 2017 Rightword Enterprises. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
